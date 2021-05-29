e1509aef694a67a5eb52e91ac28a478847097682
Juan Jose Peralta <jperalta@pixel2html.net>
2021-05-29 11:41:38 -0300

    home / seccion inscripcion lista sin media queries

---
+++ src/components/hero/hero.js
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/sections/home/inscripcion.js
@@ -0,0 +1,14 @@
+const Inscripcion = () => {
+  return (
+    <div className="inscripcion container">
+      <p className="inscript__text">se requiere acreditación previa</p>
+      <button className="inscript__button">Inscribite</button>
+      <div className="inscript__hashtags">
+        <p>#VamosAlEncuentro</p>
+        <p>#EncuentroAnualACDE</p>
+      </div>
+    </div>
+  );
+};
+
+export default Inscripcion;
