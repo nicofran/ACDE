60255a4434b0851ee51394f39597c41aea2b4cb8
Juan Jose Peralta <jperalta@pixel2html.net>
2021-05-29 12:21:45 -0300

    seccion acreditacion lista

--- src/sections/home/inscripcion.js
+++ src/sections/home/inscripcion.js
@@ -2,10 +2,12 @@ const Inscripcion = () => {
   return (
     <div className="inscripcion container">
       <p className="inscript__text">se requiere acreditación previa</p>
-      <button className="inscript__button">Inscribite</button>
-      <div className="inscript__hashtags">
-        <p>#VamosAlEncuentro</p>
-        <p>#EncuentroAnualACDE</p>
+      <div className="inscript__media">
+        <button className="inscript__button">Inscribite</button>
+        <div className="inscript__hashtags">
+          <p>#VamosAlEncuentro</p>
+          <p>#EncuentroAnualACDE</p>
+        </div>
       </div>
     </div>
   );
