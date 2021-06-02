5985151b3281435072a37d91537da0450feef509
Juan Jose Peralta <jperalta@pixel2html.net>
2021-06-01 22:46:40 -0300

    Components / menu / listo

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,17 +1,23 @@
+//IMAGES & BANNER
 import banner from "../../assets/images/banner.mp4";
 import thumbnnail from "../../assets/images/banner-thumneil.png";
 
+//COMPONENTS
+import Nav from "../../components/menu/index";
 const Hero = () => {
   return (
-    <div className=" hero container">
-      <video
-        src={banner}
-        width="70%"
-        height="600"
-        poster={thumbnnail}
-        loop
-        autoplay="autoplay"
-      ></video>
+    <div className="container">
+      <Nav />
+      <div className="hero">
+        <video
+          src={banner}
+          width="70%"
+          height="600"
+          poster={thumbnnail}
+          loop
+          autoplay
+        ></video>
+      </div>
     </div>
   );
 };
--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,5 +1,53 @@
+//Images as Components
+
+import { ReactComponent as Acde } from "../../assets/images/logo-acde1.svg";
+import bicentenario from "../../assets/images/bicentenario-menu1.png";
+import Banner from "../../assets/images/banner-thumneil.png";
+
+import { useState } from "react";
+
 const Menu = () => {
-  return <div></div>;
+  return (
+    <div className="menu">
+      <div className="menu__banners">
+        <img src={Banner} alt="ACDE" width="431" height="120" />
+        <p className="menu__fecha">
+          30 de Junio y 1<br></br>
+          de Julio<br></br>
+          <span>
+            Encuentro Virtual<br></br>Sin Cargo
+          </span>
+        </p>
+        <img src={bicentenario} alt="ACDE"></img>
+        <Acde />
+      </div>
+      <div>
+        <ul className="menu__list">
+          <li>
+            <a href="#carta">Carta al Presidente</a>
+          </li>
+          <li>
+            <a href="#hero">Programa</a>
+          </li>
+          <li>
+            <a href="#Expositores">Expositores</a>
+          </li>
+          <li>
+            <a href="#Comision">Comisión Organizadora</a>
+          </li>
+          <li>
+            <a href="#Auspiciantes">Auspicientes</a>
+          </li>
+          <li>
+            <a href="#Camino">Camino al encuentro</a>
+          </li>
+          <li>
+            <a href="#Encuentros">Encuentros Anteriores</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
