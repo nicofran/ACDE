6e2dfd1636147f908475aa9f6e17b797af7b1b66
Nacho Fernandez Peralta <66188006+ignaciofpqr@users.noreply.github.com>
2021-06-03 02:06:20 -0300

    Carta del presidente

--- src/sections/home/carta-presidente.js
+++ src/sections/home/carta-presidente.js
@@ -13,143 +13,26 @@ const Carta = () => {
       </div>
       <div className="carta__text">
         <p>
-          Muchas gracias a todos los que decidieron dedicar esta mañana fría de
-          invierno a reflexionar juntos sobre este momento tan especial en la
-          historia de la humanidad y de nuestra Patria. La Argentina atraviesa
-          una nueva crisis, desconocida en muchos aspectos, pero recurrente en
-          muchos otros, y hoy la sociedad nos interpela, casi diría “nos pide a
-          gritos” a los dirigentes empresarios, políticos, sociales y religiosos
-          que encontremos soluciones nuevas para volver a vivir en un país del
-          que estemos orgullosos y del que nuestros hijos y nietos no quieran
-          irse. La deuda real con la sociedad: la pobreza y la falta de empleo
-          digno. Que haya un 50% de población en la pobreza es una verdadera
-          tragedia. Pero no olvidemos que cada comercio, Pyme, emprendedor o
-          empresa grande que cierra o no puede seguir creciendo es también una
-          tragedia, y es, en parte la causa de esa otra tragedia. Por cada
-          empresa que cierra hay personas que se quedan sin trabajo, hay
-          ciudadanos que se quedan sin bienes y servicios, hay una cadena de
-          pagos que se rompe y hay impuestos que no se pagan.
-          <br></br>
-          <br></br>
-          Las empresas no pueden ni deben ser descartables, porque son
-          fundamentalmente organizaciones humanas integradas por personas que
-          son sus colaboradores, empleados, accionistas, directivos y sus
-          familias, pero que también incluye a sus clientes, proveedores, las
-          comunidades donde actúan, y a la sociedad toda porque a través del
-          pago de sus impuestos, le permiten al Estado proveer servicios
-          esenciales como la educación, la justicia, la salud y la seguridad.
-          <br></br>
-          <br></br>
-          Este Encuentro se gestó bajo la impronta de la actitud emprendedora.
-          Desde ACDE queremos resaltar y celebrar el espíritu emprendedor y la
-          capacidad creativa e innovadora del ser humano, capaz de superar
-          obstáculos con resiliencia, enfrentar grandes desafíos, descubrir
-          nuevas oportunidades.
-          <br></br>
-          <br></br>
-          Emprender es, en última instancia, el sentido bíblico y espiritual de
-          la co-Creación, en cuanto el ser humano encuentra su razón de ser en
-          el acto de continuar la acción del Creador.
-          <br></br>
-          <br></br>
-          Desde el origen de los tiempos, ese ser humano ha impulsado el cambio
-          buscando siempre algo más bueno, más bello, más útil y eso ha
-          permitido a la humanidad evolucionar. El primer emprendedor de la
-          historia fue un cavernícola, que mientras sus compañeros de cueva
-          dedicaban el tiempo a cazar presas que luego transportaban en una
-          especie de bandejas de piedra, ese “ingeniero” del neolítico vio la
-          necesidad de buscar otro método de transporte más efectivo e inventó
-          la rueda. Su creatividad nos podría impulsar a encontrar la nueva
-          “rueda” que necesita la Argentina para despegar y empezar a “rodar”
-          hacia la prosperidad.
-          <br></br>
-          <br></br>
-          El rol del emprendedor que se transforma en empresario y el del
-          empresario que nunca debe dejar de ser emprendedor lleva implícito
-          esta idea de la co creación, porque la esencia de la empresa es crear
-          valor para mejorar la calidad de vida de las personas, y ser célula
-          vital de la sociedad.
-          <br></br>
-          <br></br>
-          “Las sociedades que avanzan son las que ven al empresario como héroe,
-          no como villano”
-          <br></br>
-          <br></br>
-          Está estudiado que esas sociedades. valoran la iniciativa individual y
-          al emprendedor como alguien que toma riesgos, invierte, crea empleo,
-          innova, genera riqueza. Aprovechan, apoyan y potencian el espíritu
-          emprendedor que es una cualidad difícil de conseguir, y no puede ser
-          desperdiciada. En esas sociedades, los ciudadanos y los gobernantes
-          creen en la libertad de las personas, que comienza con la libertad
-          para nacer, crecer, educarse, pensar, expresarse y crear y hacer
-          crecer empresas.
-          <br></br>
-          <br></br>
-          Como señala el Profesor Carlos Newland, del ESADE, para Juan Bautista
-          Alberdi, el gran inspirador de nuestra Constitución Nacional, el
-          empresario puede ser un héroe. El buen empresario es siempre un héroe.
-          Esta opinión mucho tuvo que ver con su visión general de la necesidad
-          de desarrollar económicamente a las nueva República Argentina. ¿Cuáles
-          eran, para Alberdi, las características de un buen empresario desde el
-          punto de vista social? En primer lugar, destacaba que debían actuar
-          con independencia de injerencias políticas y burocráticas, y por sobre
-          todas las cosas no participar en entramados corruptos. En esta
-          cuestión la impronta ética era fundamental. El empresario debía
-          desenvolverse en un marco competitivo, aun cuando esto pudiera ir en
-          contra de su rentabilidad personal. Sólo cuando las ganancias
-          individuales estuvieran alineadas con las ganancias sociales se daba
-          el óptimo de bienestar.
-          <br></br>
-          <br></br>
-          En un momento como el actual donde vamos a tener que reparar el enorme
-          daño que produjo la pandemia económica y reconstruirnos, la sociedad y
-          los gobernantes no solo no deberían atacar a la actividad emprendedora
-          y empresaria, sino cuidarla, contenerla, alentarla y casi diría hasta
-          “mimarla”. La mejor prueba de la necesidad de una actividad
-          empresarial dinámica y pujante es lo que está pasando en estos más de
-          100 días en que por efecto del Rey Corona y la cuarentena, se ha
-          puesto al entramado económico en el “freezer” con el consecuente
-          deterioro social, aumento de la pobreza, el desempleo, el cierre de
-          Pymes y una baja sustancial en la calidad de vida de la sociedad.
-          <br></br>
-          <br></br>
-          Necesitamos desplegar soluciones creativas, innovadoras, aprovechando
-          hoy la potencia de la tecnología, para responder a los desafíos
-          acuciantes que enfrenta nuestra Patria y para ello los empresarios
-          responsables de su triple impacto, económico, social y ambiental son
-          indispensables.
-          <br></br>
-          <br></br>
-          Esta mirada coincide con el mensaje evangélico sobre la
-          responsabilidad del empresario cristiano que pone a la persona humana
-          en el centro de la actividad económica.
-          <br></br>
-          <br></br>Y es justamente lo que intentamos hacer en este Encuentro,
-          mostrando las acciones que los hombres y mujeres de empresa, desde las
-          más grandes hasta las más pequeñas, con una rápida y eficiente
-          reacción solidaria están llevando adelante durante la crisis en forma
-          colaborativa con el sector público y social, y como, con una
-          estrategia de resistencia y re invención, se preparan para el día
-          después.
-          <br></br>
-          <br></br>
-          Los emprendedores y empresarios y nuestros trabajadores y
-          colaboradores, estamos listos y dispuestos, pero necesitamos que la
-          sociedad y los gobernantes confíen en nosotros para ayudar a poner al
-          país en marcha nuevamente y caminar hacia el futuro.
-          <br></br>
-          <br></br>Y necesitamos también tener la certeza de la consolidación de
-          las instituciones republicanas, ya que la sola existencia de un estado
-          democrático no asegura el estado de derecho. Esta crisis debería ser
-          el punto bisagra para que la democracia dé paso a la Republica,
-          haciéndose confiable, bajo el imperio de la ley y el respeto de las
-          libertades individuales, políticas y económicas.
-          <br></br>
-          <br></br>
-          Para ello creemos desde ACDE, que no es a través de posturas extremas
-          ni de permanente confrontación, que encontraremos la salida a esta
-          crisis, sino a través del diálogo, pero un diálogo sincero, profundo,
-          basado en la verdad y en la vocación genuina por el bien común.
+          <p>
+            Muchas gracias a todos los que decidieron dedicar esta mañana fría de
+            invierno a reflexionar juntos sobre este momento tan especial en la
+            historia de la humanidad y de nuestra Patria. 
+          </p>
+          <p>
+            Sostener un camino de desarrollo para nuestro país, empieza por lograr ver más allá de las etiquetas que ponemos sobre los otros y aquellas que lo demás nos ponen a nosotros. Tenemos que ser capaces de ver más allá de lo que cimienta la desunión, para distinguir la parte de verdad que reside en el otro. Nos debemos plantear la posible combinación de múltiples verdades que se dicen y repiten en forma permanente. La igualdad de oportunidades va de la mano del valor del esfuerzo y el mérito y de hecho los aumenta. Hay que enseñar a pescar, pero las parábolas nos enseñan que existen situaciones en las que también hay que ofrecer el pescado.
+          </p>
+          <p>
+            Debemos integrarnos de forma (muy) inteligente al mundo, del mismo modo que saber vivir con lo nuestro, potenciando nuestros activos y capacidades - que son muchas. En época de pandemia, debemos proteger la vida pero también la forma de ganarse la vida.
+          </p>
+          <p>
+            Durante nuestro XXIV Encuentro Anual buscaremos responder varias preguntas para iluminar un camino de encuentro en torno a un capitalismo más humano: ¿Qué debemos hacer mejor o distinto? ¿Cómo salimos de esta tríada de incertidumbre, desesperanza y desconfianza? Los argentinos partimos de una buena base como sociedad, pero también tenemos que ajustar nuestra brújula moral. Demasiadas veces calificamos como buenas o aceptables cuestiones que son nocivas: la viveza criolla que nos trajo la pobreza criolla, el seguir más a las personas que a las ideas, la inacción o pasividad ante la conducta inmoral, el buscar imponer nuestra libertad y nuestros derechos por sobre la libertad y los derechos del otro. Queda en cada uno de quienes adherimos a profundos valores cristianos y humanos - y en especial a quienes tenemos un rol de liderazgo - potenciar una construcción de abajo hacia arriba, de cada uno en su metro cuadrado de influencia - una tarea literalmente de todos los días y a toda hora. ¿Qué esperanza puedo transmitir a los que me rodean? ¿Cómo puedo crear mayor certidumbre para los que dependen de mi?.
+          </p>
+          <p>
+            En sus últimos meses de vida, allá por 1962, nuestro fundador Enrique Shaw participó de una reunión de la Organización Internacional Católica. En ese momento ya estaba postrado en cama por su enfermedad, por lo que debió concurrir apoyado en el brazo de su hijo José Enrique. Allí después de escuchar al conferenciante describir con trazos muy pesimistas la situación social de Argentina, Enrique pidió la palabra y dijo - y hoy nos dice a nosotros:
+          </p>
+          <p>
+            "Bienaventurada es nuestra patria porque en ella un cristiano puede llegar a ser santo. Bienaventuranza es una palabra que quiere decir felicidad, y con ese significado la explicó nuestro Señor Jesucristo en el Sermón de la Montaña. Felices somos, pues, nosotros, los argentinos que podemos cumplir todas y cada una de esas bienaventuranzas. Hay mucho de bueno que hacer en nuestro país aun cuando a veces no lo parezca. Está a nuestro alcance y posibilidades hacer triunfar el cristianismo, y, por lo tanto, debemos hacer lo que ello contribuya de un modo u otro. Si empleamos bien todos los medios existentes a nuestra disposición, somos más fuertes de lo que pensamos. ¡Actuemos!"
+          </p> 
           <span className="carta__firma">
             <p>IGNACIO GORUPICZ</p>
             <p>PRESIDENTE DEL XXIV ENCUENTRO ANUAL DE ACDE</p>
