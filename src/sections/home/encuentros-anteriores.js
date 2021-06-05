9b32f84b66de4b9975033c1fbfcfc0d84b17a460
Nacho Fernandez Peralta <66188006+ignaciofpqr@users.noreply.github.com>
2021-06-05 06:05:11 -0300

    Cambios en expositores, auspiciantes y encuentros

--- src/sections/home/encuentros-anteriores.js
+++ src/sections/home/encuentros-anteriores.js
@@ -14,121 +14,104 @@ const Encuentros = () => {
         </div>
         <div className="encuentros__content">
           <ul>
+            <a href="http://www.encuentroanualacde.com.ar/acde2020/">
+              <li>XXIII: #Emprender: #Co-crear para reconstruir | 2020</li>
+            </a>
             <a href="http://www.encuentroanualacde.com.ar/acde2019/">
-              <li>XXII : La Argentina Posible | 2019</li>
+              <li>XXII: «La Argentina Posible» | 2019</li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2018/">
-              <li>XXI : Hacia una Argentina integral y plural | 2018</li>
+              <li>XXI: «Hacia una Argentina integrada y plural» | 2018</li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2017/">
               <li>
-                XX : Trabajo e inclusión: desafios frente al conocimiento y la
-                innovacion | 2017
+                XX: «Trabajo e inclusión: desafíos frente al conocimiento y la
+                innovación» | 2017
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2016/">
-              <li>XIX : Compromiso empresario: valores en accion | 2016</li>
+              <li>XIX: "Compromiso empresario: valores en acción" | 2016</li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2015/">
               <li>
-                XVIII : El valor de la Justicia. Una condición para el
-                desarrollo social | 2015
+                XVIII: «El valor de la Justicia. Una condición para el
+                desarrollo social» | 2015
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2014/">
               <li>
-                XVII : | Argentina sin Atajos. Nuestro compromiso como
-                empresarios y cuidadanos | 2014
+                XVII: «Argentina sin Atajos. Nuestro compromiso como
+                empresarios y cuidadanos» | 2014
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2013/">
-              <li>XVI : | ¿ Cómo respondemos al Papa Francisco ? 2013</li>
+              <li>XVI: «¿Cómo respondemos al Papa Francisco?» | 2013</li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2012/">
               <li>
-                XV : Los Empresarios. El Estado y un mundo en pleno cambio |
+                XV: "Los Empresarios, el Estado y un mundo en pleno cambio" |
                 2012
               </li>
             </a>
-            <br></br>
-          </ul>
-          <ul>
             <a href="http://www.encuentroanualacde.com.ar/acde2011/">
               <li>
                 XIV : Argentina: nuestros desafios para un progreso sostenible |
                 2011
               </li>
             </a>
-            <br></br>
+          </ul>
+          <ul>
             <a href="http://www.encuentroanualacde.com.ar/acde2010/">
               <li>
-                XIII : Argentina despues del Bicentenario: los liderazgos para
-                el progreso | 2010
+                XIII: "Argentina después del Bicentenario: los liderazgos para
+                el progreso" | 2010
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2009/">
               <li>
-                XII : " De la crisis al progreso. El desafio de los dirigentes
-                "| 2009
+                XII: "De la crisis, al progreso. El desafío de los dirigentes" | 2009
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2008/">
               <li>
-                XI : | Argentina: una nación para construir ¿ Que país queremos
-                los Argentinos ? 2008
+                XI: "Argentina: una nación por construir ¿Que país queremos
+                los argentinos? | 2008
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2007/">
               <li>
-                X : Bases para el progreso argentino ¿ Cual es nuestra
-                responsabilidad como dirigentes ? | 2007
+                X: "Bases para el progreso argentino ¿Cuál es nuestra
+                responsabilidad como dirigentes?" | 2007
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2006/">
               <li>
-                IX : | Iniciativas productivas para una inclusion social
-                sustentable | 2006
+                IX: "Iniciativas productivas para una inclusión social
+                sustentable" | 2006
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2005/">
               <li>
-                VIII : Desarrollando en conjunto una vision de pais | 2005
+                VIII: "Desarrollando en conjunto, una visión de pais" | 2005
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2004/">
-              <li>VII : "La generacion de trabajo en la Argentina " | 2004</li>
+              <li>VII: "La generación de trabajo en la Argentina" | 2004</li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2003/">
-              <li>VI : Hacia una verdadera Nación | 2003</li>
+              <li>VI: "Hacia una verdadera Nación" | 2003</li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2002/">
               <li>
-                V : Equidad y Solidaridad: desafios empresarios para el siglo
-                XXI | 2002
+                V: "Equidad y Solidaridad: desafíos empresarios para el siglo
+                XXI" | 2002
               </li>
             </a>
-            <br></br>
             <a href="http://www.encuentroanualacde.com.ar/acde2001/">
               <li>
-                IV : "E espiritu emprendedor como motor de conocimiento" | 2001
+                IV: "El espíritu emprendedor como motor del conocimiento" | 2001
               </li>
             </a>
-            <br></br>
           </ul>
         </div>
       </div>
