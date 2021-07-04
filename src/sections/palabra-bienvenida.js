fa7671fb475ebca34457a2b24acd447bbf595b55
Juan Jose Peralta <jperalta@pixel2html.net>
2021-07-04 19:22:28 -0300

    palabra de cierre

--- src/sections/palabra-bienvenida.js
+++ src/sections/palabra-bienvenida.js
@@ -1,6 +1,6 @@
 //PRESIDENTE DEL ENCUENTRO
 import ignacio_gorupicz from "../assets/images/ignacio_gorupicz2.png";
-//
+import gonzalo_tanoira from "../assets/images/gonzalo_tanoira.png";
 import logo from "../assets/icons/logo-acde.png";
 
 const PalabraBienvenida = () => {
@@ -178,7 +178,6 @@ const PalabraBienvenida = () => {
               podamos tener la lista de ingredientes para crear Trabajo con
               Mayúsculas en Argentina.{" "}
             </p>
-            Vamos al Encuentro!
           </p>
           <br></br>
           <br></br>
@@ -188,6 +187,337 @@ const PalabraBienvenida = () => {
               <span></span>IGNACIO GORUPICZ<br></br>
               <span>Presidente</span> <br></br>
               <span>XXIV Encuentro Anual ACDE</span>
+            </p>
+          </div>
+        </p>
+      </div>
+      <div className="bienvenida__span bienvenida__span--cierre">
+        <img className="logo" src={logo} alt="ACDE" />
+        <h1 className="bienvenida__titulo">Palabras de cierre</h1>
+      </div>
+      <div className="bienvenida__text">
+        <p>
+          <p className="parrafo_inicial">
+            <img
+              src={gonzalo_tanoira}
+              alt="Ignacio Gorupicz"
+              className="foto_presidente"
+            />
+            <br></br> Antes de cerrar este encuentro me gustaría agradecer muy
+            especialmente al equipo que lo hizo posible, encabezado por Ignacio
+            Gorupicz. Lograron realizar un gran Encuentro Anual, del que nos
+            vamos a acordar por mucho tiempo. <br />
+            <p />
+            También a los auspiciantes, que nos han acompañado con su valiosa
+            presencia en estas dos jornadas de trabajo; y muy especialmente a
+            nuestros socios, que con su aporte sostienen a un equipo de gente de
+            excelencia que tenemos llevando adelante nuestra asociación.
+            <br></br>
+            <br></br>
+            También me gustaría agradecer muy especialmente a todos los
+            disertantes, en especial a nuestro Papa Francisco, que tan
+            desinteresadamente se han preparado y nos han honrado con su
+            presencia y enriquecido con sus palabras. Entre todos han dejado muy
+            alta la vara de los encuentros anuales de nuestra asociación
+            cristiana de dirigentes de empresa.
+            <br></br>
+            <br></br>
+            El mundo ha debatido durante los últimos 100 años cuál es el modelo
+            que lleva a las naciones al desarrollo y el bienestar a las
+            personas. El eterno debate entre la derecha y la izquierda, entre
+            agrandar o dividir la torta, entre proteger a los más débiles o
+            promover a los que pueden generar trabajos, sigue siendo acalorado y
+            genera divisiones. En Argentina sobre todo, pero también en el resto
+            del mundo.
+            <br></br>
+            <br></br>
+            <p>
+              Si bien quedó demostrado que en el mundo las alternativas al
+              capitalismo fracasaron hace rato, también es cierto que este
+              necesita seguir reinventándose para poder hacer llegar sus
+              beneficios a un mayor número de personas.
+            </p>
+            <br></br>
+            <p>
+              Ya antes de la pandemia el mercado del trabajo era puesto a prueba
+              una vez más por los avances de la tecnología. Por mucho que nos
+              cueste a los empresarios, ya antes de la pandemia las reglas del
+              capitalismo estaban profundizando los niveles de desigualdad
+              mundiales.
+            </p>
+            <br></br>
+            <p>
+              Y llegó la pandemia. Nos chocó de frente de una manera a la que no
+              podíamos habernos anticipado. Llegó como una amenaza a la
+              globalización, un freno a nuestras ansias de crecimiento y
+              desarrollo. Vimos planes frustrados, personas queridas partir
+              antes de tiempo, sueños rotos.
+            </p>
+            <br></br>
+            <p>
+              Pero esa pausa también nos hizo ver que quizás el mundo estaba
+              girando demasiado rápido. Vimos que la manera en la que nos
+              estábamos manejando quizás nos hacía chocar de frente con un
+              desastre medioambiental cien veces mayor que una pandemia.
+              Entendimos que las desigualdades económicas no son un efecto
+              colateral inevitable del mundo del sálvese quien pueda. Si la
+              desigualdad es un tema relevante para el 80% de la población,
+              entonces pasa a ser un tema relevante para todos.
+            </p>
+            <br></br>
+            <p>
+              No existen burbujas individuales, en esta pandemia entendimos que
+              todos vivimos en el mismo planeta y que la suerte de algunos es
+              finalmente la suerte de todos.
+            </p>
+            <br></br>
+            <p>
+              El sector empresario viene emprendiendo a nivel global un profundo
+              cambio de hábitos. Cuando Enrique Shaw hablaba en 1950 de sus
+              propuestas era considerado un precursor. Tenemos que humanizar las
+              fábricas nos decía
+            </p>
+            <br></br>
+            <p>
+              Hoy las gerencias de Sustentabilidad, las inversiones de impacto,
+              los planes de carrera para nuestros colaboradores, las acciones
+              por las comunidades son moneda corriente en casi todas las
+              empresas.
+            </p>
+            <p>
+              Gracias a los testimonios de Enrique, de quien este año se cumplen
+              100 años de su nacimiento, a sus obras y a sus inquietudes y a las
+              de muchos otros empresarios responsables, el mundo hoy es un lugar
+              mejor para que millones de personas puedan progresar y tener una
+              vida digna.
+            </p>
+            <p>
+              Pero aun así, todavía falta mucho por hacer para lograr tener un
+              capitalismo que contemple al ser humano en toda su dimensión.
+            </p>
+            <p>
+              La pandemia, junto con el ultimátum ambiental al que nos
+              enfrentamos y al avance exponencial de la tecnología, nos obligan
+              a repensar como será el mundo a partir del día después. Podemos
+              pensar en una hoja en blanco. Tenemos una nueva oportunidad para
+              diseñar el capitalismo mundial, y por cierto el de nuestro país,
+              desde cero, de hacer una nueva receta.
+            </p>
+            <p>
+              Y como en cualquier receta, para diseñar algo conviene empezar por
+              los ingredientes, que a mi humilde criterio serían los siguientes:
+            </p>
+            <p>
+              En primer lugar, la <strong>libertad</strong>. No hay sistema
+              económico que prospere que no ponga a la libertad del ser humano
+              como la primera premisa ineludible. Libertad para elegir cómo
+              vivir la vida, sin tener un estado omnipresente que restrinja
+              nuestras acciones, entendiendo el viejo axioma de que la libertad
+              de uno termina donde empieza la libertad del otro.
+            </p>
+            <br></br>
+            <p>
+              El segundo ingrediente fundamental es la{" "}
+              <strong>democracia</strong>. Ha sido la única forma de gobierno
+              viable que ha encontrado la humanidad. Aun con todas sus posibles
+              mejoras, como por ejemplo la Boleta Única de Papel para tener
+              sistemas de votación más transparentes, y mecanismos de
+              financiamiento de las campañas electorales más controlables, la
+              democracia está fuera de duda como uno de los ingredientes
+              esenciales.
+            </p>
+            <br />
+            <p>
+              Un capitalismo más humano sin duda tiene que poner al
+              <strong>cuidado del medioambiente</strong> muy arriba en su lista
+              de prioridades.
+            </p>
+            <br />
+            <p>
+              Otro ingrediente esencial es la{" "}
+              <strong>defensa de la propiedad privada.</strong>
+              No podemos crear un estado de derecho que no asegure los frutos de
+              su trabajo a los habitantes de una Nación
+            </p>
+            <br />
+            <p>
+              Mantener y fomentar el espíritu emprendedor de la población con la
+              consiguiente generación de riqueza es clave. Crear valor y riqueza
+              no son malas palabras, debería ser promovido y alentado para el
+              bien de la sociedad.
+            </p>
+            <br />
+            <p>
+              La <strong>apertura</strong> al mundo para fomentar la innovación
+              y el desarrollo es también uno de los pilares ineludibles. No
+              existe ningún país en el mundo que haya logrado desarrollarse
+              cerrándose para vivir con lo propio. Aislarse con el pretexto de
+              proteger el trabajo nacional solo produce retrasos al desarrollo y
+              más falta de competitividad.
+            </p>
+            <br />
+            <p>
+              Un sistema de impuestos justo y equitativo es quizás donde existe
+              la mayor divergencia. ¿Cómo se cuida a los más necesitados
+              mientras al mismo tiempo se promueven las inversiones productivas
+              y se respeta la propiedad privada?
+            </p>
+            <br></br>
+            <p>
+              Tenemos mucho para acordar allí, manteniendo la perspectiva de que
+              Argentina ya tiene una de las cargas impositivas más altas del
+              mundo. Pero aquí, ambos lados deben hacer sacrificios. ¿Cómo se
+              distribuyen los beneficios del progreso? ¿Cuánto se retribuye al
+              capital y cuanto al trabajo? son preguntas que desde las empresas
+              tenemos que estar dispuestos a debatir, siempre intentando que
+              esto no atente en contra del espíritu innovador y la vocación de
+              invertir de la población.
+            </p>
+            <br />
+            <p>
+              Un estado ágil y eficiente, sin burocracia y que aproveche los
+              avances de la tecnología, es clave para poder reducir la presión
+              impositiva y que asi el sector privado genere inversiones
+              productivas.
+            </p>
+            <br />
+            <p>
+              Como nos dijo el Santo Padre ayer, el Capitalismo segun el mensaje
+              de Cristo esta resumido en Mateo 25, la parábola de los Talentos,
+              en la que un señor premia a sus siervos en la medida en que estos
+              hayan invertido bien sus ahorros para producir riqueza, y castiga
+              a aquel que los entierra para esconderlos.
+            </p>
+            <br />
+            <p>
+              Y ya que estamos en Mateo 25, traigo otro de sus celebres
+              versiculos que nos refiere nuestro Papa: Porque tuve hambre y me
+              distes de comer, tuve sed y me diste de beber. Esto nos muestra
+              que también la <strong>solidaridad</strong> es indudablemente otro
+              de los pilares sobre los que se levantará un capitalismo más
+              humano. Dios nos hizo seres sociales que vivimos en comunidad. Lo
+              que les pasa a los más débiles nos pasa a todos. Esto no puede ser
+              olvidado en nuestra receta.
+            </p>
+            <br />
+            <p>
+              Un espíritu de <strong>perdón</strong> tampoco puede estar ausente
+              en esta nueva sociedad. Poder cicatrizar heridas, dar vuelta la
+              página, dar una segunda oportunidad, volver a creer en alguien,
+              son todas actitudes que a lo largo de la historia han demostrado
+              ser superadoras por sobre mantener el odio, perseverar en la
+              crítica o promover los enfrentamientos.
+            </p>
+            <br />
+            <p>
+              Como demostró Inmaculée en su testimonio de hoy, otras sociedades
+              han podido superar grietas mucho más profundas y violentas que la
+              nuestra. Solo hay que proponérselo.
+            </p>
+            <br />
+            <p>
+              Una <strong>justicia independiente</strong> de los demás poderes,
+              a quienes tiene la obligación y responsabilidad de controlar, es
+              un insumo indispensable para lograr crecimiento y bienestar de la
+              población. No puede construirse confianza entre los habitantes de
+              una nación si estos no confían primero en la administración de la
+              justicia.
+            </p>
+            <br />
+            <p>
+              Pero para todo esto, es necesaria también una cierta cuota de
+              paciencia. Después de la pandemia, nuestro país va a salir
+              adelante con el sacrificio de sus habitantes, no con magia. Las
+              recetas mágicas de corto plazo como imprimir dinero o subir
+              impuestos ya sabemos dónde nos llevan.
+            </p>
+            <br />
+            <p>
+              Por último, pero quizás lo más importante de todo, lo que le da
+              sentido a todo este diseño, y lo que nos da la fuerza para pensar
+              en esta nueva sociedad del futuro, es la posibilidad de volver a
+              Dios.{" "}
+            </p>
+            <br />
+            <p>
+              Somos dirigentes Cristianos, seguimos el mensaje de Cristo.
+              Tenemos una amistad con él a través de la Gracia. En las últimas
+              décadas la humanidad se ha alejado cada vez más de Dios. Hemos
+              sacado a Dios de la discusión, pensando que la solución del mundo
+              estaba solo en nuestras manos. Lo hemos echado de nuestros
+              hogares, de los colegios, de nuestra sociedad. Hemos sentido
+              vergüenza de hablar de Él, de salir a comunicar su mensaje de
+              hermandad entre los pueblos de la tierra.
+            </p>
+            <br />
+            <p>
+              Hemos visto cómo el individualismo tomaba mayor preponderancia
+              bajo el lema del sálvese quien pueda. Un Capitalismo más humano
+              sin duda tiene que ver con volver a la esencia del mensaje de
+              Jesús, que pone al amor a Dios y al prójimo por sobre todo lo
+              demás. Hasta que no entendamos esto no podremos diseñar ese nuevo
+              capitalismo que tanto necesitamos.
+            </p>
+            <br />
+            <p>
+              ACDE ha intentado durante todo este año alzar la voz cada vez que
+              lo consideró necesario, para que nuestro país siguiera una senda
+              hacia el desarrollo y pudiera reducir la pobreza. Intentamos en
+              todo momento guiarnos por los mensajes que Cristo nos dejó, por la
+              Doctrina Social de la Iglesia y esperando siempre haber sido
+              iluminados por el Espíritu Santo en lo que decíamos.
+            </p>
+            <br />
+            <p>
+              Por ello, agradezco enormemente al Consejo Directivo que me
+              acompaña, por su apoyo, por su claridad de pensamiento y por su
+              valentía para decir siempre lo que piensan.
+            </p>
+            <br />
+            <p>
+              <strong>
+                Reconocemos que el gobierno ha tenido que enfrentar situaciones
+                inusitadamente difíciles, que no estaban en los planes de nadie.
+                Pero también nos damos cuenta, y así lo hemos expresado, que en
+                muchas ocasiones se han tomado decisiones equivocadas, que nos
+                alejan cada vez más de una posible recuperación.
+              </strong>
+            </p>
+            <br />
+            <p>
+              Estoy convencido de que, como dirigentes, tenemos que seguir
+              proponiendo caminos constructivos para salir adelante. Como
+              empresarios tenemos que seguir poniendo el hombro para generar
+              empleo productivo en el país. Y como Cristianos tenemos la
+              responsabilidad de seguir transmitiendo con entusiasmo el mensaje
+              de Cristo.
+            </p>
+            <br />
+            <p>
+              Y resalto la palabra entusiasmo, que viene del griego “En Theus”,
+              que significa en Dios. Quien tiene entusiasmo lo lleva a Dios
+              adentro. Sin duda Enrique Shaw fue quien mejor representa esto,
+              con el entusiasmo que le ponía a todo lo que se proponía
+            </p>
+            <br />
+            <p>
+              Convocamos a toda la dirigencia a continuar con el diálogo y los
+              aportes constructivos, para entre todos encontrar la mejor receta
+              para hacer un capitalismo más humano, que nos ayude a construir el
+              país que tanto necesitamos.
+            </p>
+            <br />
+            <p>
+              Que Cristo Jesus nos acompañe en este camino y que la Inmaculada
+              Madre nos proteja.
+            </p>
+            <br />
+            <p>Muchas gracias</p>
+          </p>
+          <div>
+            <p className="carta__firma">
+              <span></span>GONZALO TANOIRA<br></br>
+              <span>Presidente ACDE</span> <br></br>
             </p>
           </div>
         </p>
