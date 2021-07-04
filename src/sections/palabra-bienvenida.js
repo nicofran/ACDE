02aedf4b8a78b3b779e01fe0e1f3711c63d47cb7
Juan Jose Peralta <jperalta@pixel2html.net>
2021-07-04 18:37:15 -0300

    Agregue palabras de bienvenida

---
+++ src/sections/palabra-bienvenida.js
@@ -0,0 +1,199 @@
+//PRESIDENTE DEL ENCUENTRO
+import ignacio_gorupicz from "../assets/images/ignacio_gorupicz2.png";
+//
+import logo from "../assets/icons/logo-acde.png";
+
+const PalabraBienvenida = () => {
+  return (
+    <div className="bienvenida container" id="carta">
+      <div className="bienvenida__span">
+        <img className="logo" src={logo} alt="ACDE" />
+        <h1 className="bienvenida__titulo">Palabras de Bienvenida</h1>
+      </div>
+      <div className="bienvenida__text">
+        <p>
+          <p className="parrafo_inicial">
+            <img
+              src={ignacio_gorupicz}
+              alt="Ignacio Gorupicz"
+              className="foto_presidente"
+            />
+            <strong> Discurso Apertura XXIV Encuentro Anual ACDE 2021</strong>
+            <br></br>
+            <br></br> Todos tenemos el mismo sueño para Argentina – Una sociedad
+            y una economía donde haya un lugar digno en la mesa para todos. Así
+            fue como arrancó y se desarrolló la Argentina. Así es como avanzan
+            hoy los países que generan las mejores condiciones para que cada
+            persona y comunidad tenga las oportunidades y despliegue su
+            potencial. Hay algo que caracteriza a los países que progresan: hay
+            trabajo, hay mucho trabajo. Hay muchos trabajos nuevos y hay otros
+            que cambian por la tecnología. Hay tanto trabajo en estos países que
+            actúan como un imán que atrae trabajadores y sus familias de otros
+            países. <br />
+            <p />
+            Junto con un equipo organizador al que le estoy muy agradecido,
+            iniciamos la preparación de este encuentro el año pasado. En ese
+            momento de incertidumbre, decidimos hacer una encuesta a los socios
+            jóvenes de ACDE. Nos alegró encontrar que más de la mitad era
+            optimista sobre el futuro. Nos llamó la atención que muchas
+            respuestas versaban sobre lo mismo: aparentemente hay dos acepciones
+            de la palabra contexto. Por un lado, hay un contexto macro que en
+            general no ayuda ni inspira. Un contexto macro que muchas veces
+            desanima – un lugar donde pareciera que hay que “remar en dulce de
+            leche”. Pero hay otro contexto cercano: es uno más positivo donde
+            hay oportunidad y donde hay mucho por hacer. Volviendo a la analogía
+            del deporte, es en este contexto cercano donde buscamos remar
+            sincronizados con los que tenemos alrededor. Una de las respuestas
+            ilustraba acabadamente esto de los dos contextos y se las quiero
+            transcribir literal: “Porque depende de mí lo que vaya a hacer. Si
+            espero al contexto macro no asumo mi responsabilidad”.
+            <br></br>
+            <br></br>
+            <strong>
+              El desafío que tenemos como país es que nos falta Encuentro
+            </strong>{" "}
+            – Es un desafío especialmente importante para un país cuyo lema
+            fundacional desde hace más de 200 años es “En Unión y Libertad”. Nos
+            está costando esa unión de voluntades libres para constituir ese
+            sueño común que es la Argentina. El primer paso para superar este
+            desafío es lograr ver más allá de las etiquetas que ponemos sobre
+            los otros y aquellas etiquetas que nos ponen a nosotros. Nos debemos
+            plantear la posible combinación de varias verdades que se repiten a
+            menudo. Hay que enseñar a pescar, pero las parábolas nos enseñan que
+            existen muchas situaciones en las que también hay que ofrecer el
+            pescado. Debemos integrarnos de forma muy inteligente al mundo, del
+            mismo modo que saber vivir con lo nuestro, potenciando nuestros
+            activos y capacidades – que son muchas. La igualdad de oportunidades
+            va de la mano del valor del esfuerzo y del mérito y de hecho los
+            aumenta. Lo opuesto a la igualdad de oportunidades en realidad son
+            los privilegios y la corrupción. Pero bien nos decía Enrique Shaw
+            que la Justicia sola no puede, necesitamos también la Caridad
+            Social.
+            <br></br>
+            <br></br>
+            No quiero profundizar en la naturaleza de este desafío en torno al
+            encuentro de los argentinos. Creo que si algo nos propusimos para
+            estas dos mañanas que tenemos por delante es mirar menos al pasado y
+            más al futuro. Tenemos que enfrentar los desafíos con respuestas más
+            que con explicaciones y lamentos. Si no tenemos todas las respuestas
+            al menos ofrecer un camino concreto a transitar.
+            <br></br>
+            <br></br>
+            <p>
+              <strong>
+                Una causa donde se entrelazan la promoción del bien común y la
+                construcción de confianza es la generación de Trabajo
+              </strong>
+              – Trabajo con mayúsculas: registrado, productivo y de calidad. La
+              Organización Internacional del Trabajo lo describe con el adjetivo
+              de decente. Este tema del trabajo decente, trillado pero no por
+              eso menos valido, plantea esta dinámica de contraposición entre
+              ese sueño que compartimos y ese desafío del desencuentro. Si
+              partimos de la premisa de que más trabajo es menos pobreza y por
+              lo tanto más inclusión, surge una pregunta. ¿Qué tan tierra fértil
+              es Argentina para generar este trabajo decente? Vayamos repasando
+              al menos 12 ingredientes de la receta probada para crear trabajo
+              decente: (i) contexto macro previsible, (ii) educación para la
+              empleabilidad, (iii) igualdad de género, (iv) seguridad jurídica
+              para invertir en crear trabajo, (v) cuidado del medio ambiente y
+              economía verde, (vi) libertad de contratación, (vii) condiciones
+              de trabajo saludables y seguras, (viii) adaptabilidad al cambio
+              tecnológico, (ix) umbrales mínimos de ingreso por arriba de la
+              línea de la pobreza, (x) menor presión impositiva en las escalas
+              más bajas para que haya mayor ingreso disponible para el
+              trabajador, (xi) foco en desarrollo de todo tipo de trabajos para
+              todos los niveles socioeconómicos (xii) marco legal justo que
+              proteja de los abusos y la extorsión. Listar estos ingredientes
+              nos ayuda a dimensionar lo complejo que es generar trabajo
+              decente. Si respondemos esta pregunta con honestidad intelectual
+              posiblemente concluiremos que no hay solución fácil ni atajos. Al
+              terminar volveré sobre esta lista.
+            </p>
+            <br></br>
+            <p>
+              En el libro del Éxodo se cuenta que el pueblo de Israel guiado por
+              Moises enfrenta al pueblo nómade de los amalecitas en el medio del
+              desierto. Ocurría que mientras Moisés tenía en alto la mano vencía
+              Israel, mientras que cuando bajaba la mano vencían los amalecitas.
+              Como a Moises le pesaban las manos, dos de sus ayudantes tomaron
+              una piedra y se la pusieron debajo para que se sentase y luego le
+              sostenían los brazos, uno a cada lado. Así Moises sostuvo los
+              brazos firmes hasta la puesta del sol y la victoria en esa batalla
+              en el desierto. Esta historia tiene muchas enseñanzas. Rescato dos
+              que me parecen oportunas para el momento que nos toca: no solo no
+              tenemos que bajar los brazos sino también tenemos que sostenernos
+              mutuamente.
+            </p>
+            <br></br>
+            <p>
+              Lo bueno de plantear esta causa en torno a generar trabajo decente
+              es que nos orienta a poner los principios cristianos en
+              movimiento. Menciono algunos de estos principios tan centrales en
+              las exhortaciones del Papa Francisco y de otros líderes
+              religiosos: el principio del bien común, el principio de
+              subsidiariedad que fomenta la toma de iniciativa tan propia del
+              espíritu humano, el principio de solidaridad y los valores de
+              verdad, libertad, justicia y amor. Creo hace más sentido hablar de
+              militancia de estos valores. Tenemos muchas formas de poner estas
+              palabras en acción más allá de la empresa. También tenemos que
+              militar estos valores en nuestro barrio y comunidades, en las
+              periferias que tenemos cerca y donde hay tanta pobreza, en la
+              educación, en la política, en todo ámbito del trabajo y de la
+              cultura. Seguramente los que estamos compartiendo este encuentro
+              tenemos muchas ocupaciones e intentamos poner en acción varios de
+              estos valores – ¿Qué más puedo hacer? ¿Qué puedo hacer mejor?
+              Visualicemos a las personas alrededor nuestro: los que trabajan
+              con nosotros y también quienes trabajan en nuestros hogares.
+              Miremos los lugares por los que transcurrimos: el colegio de
+              nuestros hijos, pero también otros colegios, los lugares con más
+              pobreza y en esta época de elecciones los lugares donde se va a
+              votar. Esta militancia en valores es algo de todos los días. No se
+              requiere impacto a escala sino solo el compromiso de hacer lo
+              correcto porque es lo correcto. Atahualpa Yupanqui decía que la
+              arena es un puñadito, pero hay montañas de arena.
+            </p>
+            <br></br>
+            <p>
+              <strong>
+                Nuestro fundador Enrique Shaw nos dejó una hoja de ruta para ir
+                hacia ese capitalismo más humano que queremos para Argentina
+              </strong>
+              . Esa hoja de ruta no está solamente en sus palabras, sino
+              principalmente en su modo de vivir . Enrique vivió hasta los 41
+              años, pero fue un militante en valores en espíritu y en verdad.
+              Junto con empresarios y ejecutivos, tuvo la visión de largo
+              alcance y el empuje de fundar ACDE para lograr llevar el mensaje
+              de Cristo a la empresa y humanizar la fábrica. Es en sus últimos
+              años cuando su obra empieza a tomar volumen y es lo que permite
+              que su legado haya trascendido y tenga la vigencia que tiene hoy.
+              Seguramente Enrique sabía que el que achica los sueños, achica las
+              realidades.
+            </p>
+            <br></br>
+            <p>
+              Aceptemos que a veces es preciso perderse para volver a
+              encontrarse. En vez de mirar lo que fuimos miremos hacia el futuro
+              con la convicción de que podemos ser mejores. Hay mucho de bueno
+              que hacer en nuestro país. Les pido que al final de estos dos días
+              podamos tener la lista de ingredientes para crear Trabajo con
+              Mayúsculas en Argentina.{" "}
+            </p>
+            Vamos al Encuentro!
+          </p>
+          <br></br>
+          <br></br>
+          <p>Muchas gracias</p>
+          <div>
+            <p className="carta__firma">
+              <span></span>IGNACIO GORUPICZ<br></br>
+              <span>Presidente</span> <br></br>
+              <span>XXIV Encuentro Anual ACDE</span>
+            </p>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PalabraBienvenida;
